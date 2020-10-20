6eb2a2c36113c5809d3681e9fef231f9dd58a92b
amar0898 <70146865+amar0898@users.noreply.github.com>
2020-10-20 12:45:49 +0530

    Update Launcher.py

--- Launcher.py
+++ Launcher.py
@@ -32,10 +32,10 @@ migrate=Migrate(app,db)
 
 class Contacts(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(60), nullable=False)
     phone_num = db.Column(db.String(12), nullable=False)
     msg = db.Column(db.String(120), nullable=False)
-    date = db.Column(db.String(12), nullable=True)
+    date = db.Column(db.String(60), nullable=True)
     email = db.Column(db.String(20), nullable=False)
 
 
